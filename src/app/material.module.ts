47af70896951172c7119d17b7d875fd40ac21232
webproger1 <Webproger1@gmail.com>
2021-03-10 15:07:04 +0200

    added material angular

---
+++ src/app/material.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatOptionModule } from '@angular/material/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+
+
+const modules = [
+  MatButtonModule,
+  MatIconModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+  MatGridListModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatSliderModule,
+];
+
+@NgModule({
+  imports: [...modules],
+  exports: [...modules]
+})
+
+export class MaterialModule {
+}
