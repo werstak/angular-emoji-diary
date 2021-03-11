bed778af44d2bb44aae26f5289e453a29183341e
webproger1 <Webproger1@gmail.com>
2021-03-11 07:40:50 +0200

    rename projekt

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularEmojiComments
+# AngularEmojiDiary
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-emoji-comments'`, () => {
+  it(`should have as title 'angular-emoji-diary'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-emoji-comments');
+    expect(app.title).toEqual('angular-emoji-diary');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-emoji-comments app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-emoji-diary app is running!');
   });
 });
